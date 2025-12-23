3f947623ee4de3960906b43b9ab444f1dd9f17d4
DeathbatO13 <danielalejo99@gmail.com>
2025-12-23 17:07:27 -0500

    Change Satate Square

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
 
 const TURNS = {
-  X: 'x',
-  O: 'o'
+  X: 'X',
+  O: 'O'
 }
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <div key={index} className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
@@ -15,7 +22,19 @@ const Square = ({ children, updateBoard, index }) => {
 
 function App() {
 
-  const [board, setBoard] = useState(['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'])
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) => {
+   
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
 
   return (
     <main className='board'>
@@ -24,12 +43,16 @@ function App() {
         {
           board.map((_, index) => {
             return (
-              <Square key={index} index={index}>
+              <Square key={index} index={index} updateBoard={updateBoard}>
                 {board[index]}
               </Square>
             )
           })
         }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   )
