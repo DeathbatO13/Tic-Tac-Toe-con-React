06ed698e0393ecafe5ecafb2f9d38b864d623968
DeathbatO13 <danielalejo99@gmail.com>
2025-12-26 07:41:13 -0500

    Change Languaje

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -9,13 +9,13 @@ export function WinnerModal ({ winner, resetGame }) {
         <section className="winner">
             <div className="text">
                 <h2>
-                    {winner === false ? 'Empate' : 'Ganó: '}
+                    {winner === false ? 'Tie' : 'Winner: '}
                 </h2>
                 <header className="win">
                     {winner && <Square>{winner}</Square>}
                 </header>
                 <footer>
-                    <button onClick={resetGame}>Empezar de nuevo</button>
+                    <button onClick={resetGame}>Start Again</button>
                 </footer>
             </div>
         </section>
